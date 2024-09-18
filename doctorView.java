28329098e8add2fa9f55b3a394920e7cc778d99a
mclovin65 <158982735+mclovin65@users.noreply.github.com>
2024-09-18 07:36:25 -0600

    Add files via upload
    creacion de doctor view

---
+++ doctorView.java
@@ -0,0 +1,72 @@
+package view;
+import javax.swing.*;
+import java.awt.*;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class doctorView extends JFrame {
+
+    // Atributo tipo Array para las dimensiones de la ventana
+    private int[] pantalla = {1300, 800};
+
+    public doctorView() {
+        // Configuración básica del JFrame con dimensiones desde el atributo pantalla
+        setTitle("Hospital Santa Catalina - Perfil del doctor");
+        setSize(pantalla[0], pantalla[1]);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
+
+        // Panel Header
+        JPanel headerPanel = new JPanel();
+        headerPanel.setPreferredSize(new Dimension(1300, 60));
+        headerPanel.setBackground(Color.DARK_GRAY);
+        headerPanel.setLayout(new BorderLayout());
+
+        // JLabel para el nombre del hospital
+        JLabel hospitalLabel = new JLabel("Hospital Santa Catalina");
+        hospitalLabel.setForeground(Color.WHITE);
+        hospitalLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        hospitalLabel.setHorizontalAlignment(SwingConstants.LEFT);
+        headerPanel.add(hospitalLabel, BorderLayout.WEST);
+
+        // Crear un panel para el nombre del doctor y su especialidad (al lado derecho)
+        JPanel doctorInfoPanel = new JPanel();
+        doctorInfoPanel.setLayout(new GridLayout(2, 1));  // 2 filas, 1 columna
+        doctorInfoPanel.setBackground(Color.DARK_GRAY);
+
+        // Etiqueta para el nombre del doctor
+        JLabel doctorNameLabel = new JLabel("fogel");
+        doctorNameLabel.setFont(new Font("Arial", Font.BOLD, 17));
+        doctorNameLabel.setForeground(Color.WHITE);
+        doctorNameLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        doctorInfoPanel.add(doctorNameLabel);
+
+        // Etiqueta para la especialidad del doctor
+        JLabel doctorSpecialtyLabel = new JLabel("Doctor General");
+        doctorSpecialtyLabel.setFont(new Font("Arial", Font.PLAIN, 14
+        ));
+        doctorSpecialtyLabel.setForeground(Color.WHITE);
+        doctorSpecialtyLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        doctorInfoPanel.add(doctorSpecialtyLabel);
+
+        // Añadir el panel con la información del doctor al lado derecho
+        headerPanel.add(doctorInfoPanel, BorderLayout.EAST);
+
+        // Añadir headerPanel al norte
+        add(headerPanel, BorderLayout.NORTH);
+
+        // Panel para la representación del usuario (userPanel)
+        JPanel userPanel = new JPanel();
+        userPanel.setLayout(new GridBagLayout());
+
+        // Añadir el panel de usuario al centro
+        add(userPanel, BorderLayout.CENTER);
+    }
+
+
+    }
+
+
+
